92afcdfdd9cfd1f2e2827371b564f3a46a74a76d
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-09-10 12:41:19 +0200

    log some gameObject properties to verify that it works

--- Assets/Luny/Scripts/Lua/Runtime/LunyRuntimeTestScript.lua
+++ Assets/Luny/Scripts/Lua/Runtime/LunyRuntimeTestScript.lua
@@ -4,4 +4,6 @@ local script = ...
 -- Unity event messages call Lua functions of the same name in the 'script' table:
 script.OnEnable = function()
 	print("Hello, " .. script.Name .. ".lua on GameObject: " .. tostring(script.gameObject))
+	print("name: " .. tostring(script.gameObject.name))
+	print("layer: " .. tostring(script.gameObject.layer))
 end
